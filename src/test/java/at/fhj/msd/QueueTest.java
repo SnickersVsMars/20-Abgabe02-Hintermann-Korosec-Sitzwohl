96011a226698fe70a62719e1c562049fc68f5f6b
Tobias Hintermann <tobiashintermann@MacBook-Pro-2.local>
2020-06-24 10:12:04 +0200

    Updated testMessages/Displayname() + implemented testReturnedPollElement()

--- src/test/java/at/fhj/msd/QueueTest.java
+++ src/test/java/at/fhj/msd/QueueTest.java
@@ -24,80 +24,99 @@ public class QueueTest {
         queue.offer("one");
         queue.offer("two");
         queue.offer("three");
-        assertFalse(queue.offer("four"), "Expected \"false\" but element got added!");
+        assertFalse(queue.offer("four"),
+                "Expected \"false\" but element got added!");
     }
 
     @Test
-    @DisplayName("Testing if adding works properly")
+    @DisplayName("Testing if offer()-ing works properly")
     void testOfferSimpleAdd() {
         queue.offer("one");
-        assertTrue(queue.offer("two"), "Expected \"true\" but element was not added!");
+        assertTrue(queue.offer("two"),
+                "Expected \"true\" but element was not added!");
     }
 
     @Test
-    @DisplayName("Testing if polling an element works properly")
+    @DisplayName("Testing if poll()-ing an element works properly")
     void testSimplePoll() {
         queue.offer("one");
         queue.offer("two");
         queue.poll();
-        assertEquals("two",queue.peek());
+        assertEquals("two",queue.peek(),
+                "Expected head to be \"two\" but was:" + queue.peek());
     }
 
     @Test
-    @DisplayName("Testing if polling works properly when size 0")
+    @DisplayName("Testing poll() when queue empty")
     void testPollButNoElement() {
-        assertNull(queue.poll());
+        assertNull(queue.poll(),
+                "Expected to be \"null\" but queue was not empty!");
     }
 
     @Test
-    @DisplayName("Remove when null to throw NoSuchElementException")
+    @DisplayName("Testing returned element of poll()")
+    void testReturnedPollElement() {
+        queue.offer("one");
+        String result = (String) queue.poll();
+        assertEquals("one", result,
+                "Expected returned element to be \"one\" but was:" + result);
+    }
+
+    @Test
+    @DisplayName("Testing remove() when null, throws NoSuchElementException")
     void testRemoveWhenNull() {
         assertThrows(NoSuchElementException.class, () -> {
             queue.remove();
-        });
+        },"Expected to throw \"NoSuchElementException\"!");
     }
 
     @Test
-    @DisplayName("Remove element from queue")
+    @DisplayName("Testing remove() from queue")
     void testRemoveWhenNotNull() {
         queue.offer("one");
         queue.offer("two");
         queue.remove();
-        assertEquals("two",queue.peek());
+        assertEquals("two",queue.peek(),
+                "Expected to be \"two\" but was:" + queue.peek());
     }
 
     @Test
     @DisplayName("Testing returned element of remove()")
     void testReturnedRemoveElement() {
         queue.offer("one");
-        assertEquals("one",queue.remove());
+        String result = (String) queue.remove();
+        assertEquals("one",result,
+                "Expected returned element to be \"one\" but was:" + result);
     }
 
     @Test
     @DisplayName("Testing peek() with element in queue")
     void testPeekWithElement() {
         queue.offer("one");
-        assertEquals("one",queue.peek());
+        assertEquals("one",queue.peek(),
+                "Expected to be \"one\" but was:" + queue.peek());
     }
 
     @Test
     @DisplayName("Testing peek() with no element")
     void testPeekNoElement() {
-        assertNull(queue.peek());
+        assertNull(queue.peek(),
+                "Expected to be \"null\" but was:" + queue.peek());
     }
 
     @Test
     @DisplayName("Testing element() with elements in queue")
     void testElementWithElement() {
         queue.offer("one");
-        assertEquals("one", queue.element());
+        assertEquals("one", queue.element(),
+                "Expected to return \"one\" but was:" + queue.element());
     }
 
     @Test
-    @DisplayName("Testing element() with no elements in queue")
+    @DisplayName("Testing element() with no elements, throws NoSuchElementException")
     void testElementException() {
             assertThrows(NoSuchElementException.class, () -> {
                 queue.element();
-            });
+            },"Expected to throw \"NoSuchElementException\"!");
     }
 }
